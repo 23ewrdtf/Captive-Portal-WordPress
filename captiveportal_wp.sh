162ce2bf653ec52c67cbc4afb3c0d9204564513d
Adam <adam.g@outlook.com>
2019-03-22 11:05:56 +0000

    Update captiveportal_wp.sh

--- captiveportal_wp.sh
+++ captiveportal_wp.sh
@@ -29,6 +29,30 @@ apt-get install nginx -yqq
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/default_nginx -O /etc/nginx/sites-enabled/default
 
 echo "┌─────────────────────────────────────────"
+echo "|Installing PHP7"
+echo "└─────────────────────────────────────────"
+apt-get install php7.0-fpm -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing and configuring MySQL"
+echo "└─────────────────────────────────────────"
+apt-get install mysql-server php-mysql -yqq
+dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
+mysql -e "$dbsetup"
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing and configuring WordPress"
+echo "└─────────────────────────────────────────"
+curl -o /var/www/html/latest.tar.gz -O https://wordpress.org/latest.tar.gz
+tar -C /var/www/html/ -zxvf /var/www/html/latest.tar.gz
+rm -rf /var/www/html/latest.tar.gz
+chown -R www-data /var/www/html/wordpress
+rm -rf /var/www/html/wordpress/wp-config-sample.php
+mkdir /var/www/html/wordpress/wp-content/uploads
+chmod 775 /var/www/html/wordpress/wp-content/uploads
+wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/wp-config.php -O /var/www/html/wordpress/wp-config.php
+
+echo "┌─────────────────────────────────────────"
 echo "|Installing dnsmasq"
 echo "└─────────────────────────────────────────"
 apt-get install dnsmasq -yqq
@@ -78,27 +102,3 @@ echo "|After the next step is complete,"
 echo "|please reboot your pi and test."
 echo "└─────────────────────────────────────────"
 read -p "Press enter to install PHP"
-
-echo "┌─────────────────────────────────────────"
-echo "|Installing PHP7"
-echo "└─────────────────────────────────────────"
-apt-get install php7.0-fpm -yqq
-
-echo "┌─────────────────────────────────────────"
-echo "|Installing and configuring MySQL"
-echo "└─────────────────────────────────────────"
-apt-get install mysql-server php-mysql -yqq
-dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
-mysql -e "$dbsetup"
-
-echo "┌─────────────────────────────────────────"
-echo "|Installing and configuring WordPress"
-echo "└─────────────────────────────────────────"
-curl -o /var/www/html/latest.tar.gz -O https://wordpress.org/latest.tar.gz
-tar -C /var/www/html/ -zxvf /var/www/html/latest.tar.gz
-rm -rf /var/www/html/latest.tar.gz
-chown -R www-data /var/www/html/wordpress
-rm -rf /var/www/html/wordpress/wp-config-sample.php
-mkdir /var/www/html/wordpress/wp-content/uploads
-chmod 775 /var/www/html/wordpress/wp-content/uploads
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/wp-config.php -O /var/www/html/wordpress/wp-config.php
