7269224e898840addcd4512faec3e9292112dab1
Adam <adam.g@outlook.com>
2019-03-22 09:23:54 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo "┌─────────────────────────────────────────"
-echo "|Configuring MySQL, Pa$$w0rd123 is temporary,
+echo "|Configuring MySQL, Pa$$w0rd123 is temporary,"
 echo "|CHANGE BEFORE RUN THIS SCRIPT"
 echo "└─────────────────────────────────────────"
 mysql -e "create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$mysqlhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
