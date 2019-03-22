68f0ec6aaaa6d787d5b6a02704f5f42e17933369
Adam <adam.g@outlook.com>
2019-03-22 10:35:00 +0000

    Update and rename captiveportal.sh to captiveportal_wp.sh

--- captiveportal_wp.sh
+++ captiveportal_wp.sh
@@ -85,6 +85,20 @@ echo "└───────────────────────�
 apt-get install php7.0-fpm -yqq
 
 echo "┌─────────────────────────────────────────"
-echo "|Installing MySQL"
+echo "|Installing and configuring MySQL"
 echo "└─────────────────────────────────────────"
 apt-get install mysql-server php-mysql -yqq
+dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
+mysql -e "$dbsetup"
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing and configuring WordPress"
+echo "└─────────────────────────────────────────"
+curl -o /var/www/html/latest.tar.gz -O https://wordpress.org/latest.tar.gz
+tar -C /var/www/html/ -zxvf /var/www/html/latest.tar.gz
+rm -rf /var/www/html/latest.tar.gz
+chown -R www-data /var/www/html/wordpress
+rm -rf /var/www/html/wordpress/wp-config-sample.php
+mkdir /var/www/html/wordpress/wp-content/uploads
+chmod 775 /var/www/html/wordpress/wp-content/uploads
+wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/wp-config.php -O /var/www/html/wordpress/wp-config.php
