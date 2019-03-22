4792befdcfe788f529bfadd8509d8bc78893efc8
Adam <adam.g@outlook.com>
2019-03-22 09:43:34 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,28 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
-dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
-mysql -e "$dbsetup"
+#dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
+#mysql -e "$dbsetup"
+
+curl -O https://wordpress.org/latest.tar.gz
+cd /var/www/html
+tar xzf latest.tar.gz
+rm -rf latest.tar.gz
+chown -R www-data /var/www/html/wordpress
+mv wp-config-sample.php wp-config.php
+perl -pi -e "s'database_name_here'"wordpress_db"'g" wp-config.php
+perl -pi -e "s'username_here'"wordpress_user"'g" wp-config.php
+perl -pi -e "s'password_here'"Pa66w0rd123"'g" wp-config.php
+perl -pi -e "s/\'wp_\'/\'wp_\'/g" wp-config.php
+#set WP salts
+perl -i -pe'
+	  BEGIN {
+	    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+	    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+	    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+	  }
+	  s/put your unique phrase here/salt()/ge
+	' wp-config.php
+#create uploads folder and set permissions
+mkdir wp-content/uploads
+chmod 775 wp-content/uploads
