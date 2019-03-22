f7655d1ef48699d1a624782ad3d52e43cdc33ec9
Adam <adam.g@outlook.com>
2019-03-22 09:44:35 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ fi
 #dbsetup="create database wordpress_db;GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$localhost IDENTIFIED BY 'Pa66w0rd123';FLUSH PRIVILEGES;"
 #mysql -e "$dbsetup"
 
-curl -O https://wordpress.org/latest.tar.gz
 cd /var/www/html
+curl -O https://wordpress.org/latest.tar.gz
 tar xzf latest.tar.gz
 rm -rf latest.tar.gz
 chown -R www-data /var/www/html/wordpress
