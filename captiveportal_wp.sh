0e3186aaa796c29cb146c43a0af78a142f4a22c0
Adam <adam.g@outlook.com>
2019-05-06 14:28:43 +0100

    Update captiveportal_wp.sh

--- captiveportal_wp.sh
+++ captiveportal_wp.sh
@@ -3,18 +3,18 @@
 echo "┌─────────────────────────────────────────"
 echo "|Updating repositories"
 echo "└─────────────────────────────────────────"
-apt-get update -yqq
+apt-get update -yqq > /dev/null
 
 echo "┌─────────────────────────────────────────"
 echo "|Installing Nginx"
 echo "└─────────────────────────────────────────"
-apt install nginx -yqq
+apt install nginx -yqq > /dev/null
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/default_nginx -O /etc/nginx/sites-enabled/default
 
 echo "┌─────────────────────────────────────────"
 echo "|Installing MySQL"
 echo "└─────────────────────────────────────────"
-apt-get install mysql-server -yqq
+apt-get install mysql-server -yqq > /dev/null
 mysql_secure_installation <<EOF
 y
 Pa55w04d123
@@ -31,8 +31,8 @@ mysql -e "$dbsetup"
 echo "┌─────────────────────────────────────────"
 echo "|Installing PHP"
 echo "└─────────────────────────────────────────"
-apt install php-fpm php-mysql -yqq
-apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -yqq
+apt install php-fpm php-mysql -yqq > /dev/null
+apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -yqq > /dev/null
 systemctl restart php7.0-fpm
 
 echo "┌─────────────────────────────────────────"
@@ -49,7 +49,7 @@ printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s wp-config.php
 echo "┌─────────────────────────────────────────"
 echo "|Installing dnsmasq"
 echo "└─────────────────────────────────────────"
-apt-get install dnsmasq -yqq
+apt-get install dnsmasq -yqq > /dev/null
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/dhcpcd.conf -O /etc/dhcpcd.conf
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/dnsmasq.conf -O /etc/dnsmasq.conf
 update-rc.d dnsmasq defaults
@@ -57,7 +57,7 @@ update-rc.d dnsmasq defaults
 echo "┌─────────────────────────────────────────"
 echo "|Installing hostapd"
 echo "└─────────────────────────────────────────"
-apt-get install hostapd -yqq
+apt-get install hostapd -yqq > /dev/null
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/hostapd.conf -O /etc/hostapd/hostapd.conf
 sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
 systemctl unmask hostapd.service
@@ -70,7 +70,7 @@ iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-
 iptables -t nat -A POSTROUTING -j MASQUERADE
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-apt-get -yqq install iptables-persistent
+apt-get -yqq install iptables-persistent > /dev/null
 
 echo "┌─────────────────────────────────────────"
 echo "|Reboot and test"
