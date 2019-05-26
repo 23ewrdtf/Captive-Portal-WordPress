885ba5b6e3a8b7940abe8c43de23b121617b468d
Adam <adam.g@outlook.com>
2019-05-26 16:55:40 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Pa55w04d123' );
+define( 'DB_PASSWORD', 'Pa55w04d123$' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
