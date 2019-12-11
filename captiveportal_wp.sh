859a34156cf476bfedef2a266b725bc790a20962
unknown <adam.g@outlook.com>
2019-12-11 09:30:50 +0000

    Updated to work with latest raspbian buster

--- captiveportal_wp.sh
+++ captiveportal_wp.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root, run sudo -i before running that script."
+	exit
+fi
+
+echo "┌─────────────────────────────────────────"
+echo "|This script might take a while,"
+echo "|so if you dont see much progress,"
+echo "|wait till you see --all done-- message."
+echo "└─────────────────────────────────────────"
+read -p "Press enter to continue"
+
 echo "┌─────────────────────────────────────────"
 echo "|Updating repositories"
 echo "└─────────────────────────────────────────"
-apt-get update -yqq > /dev/null
+apt-get update -yqq
+
+# echo "┌─────────────────────────────────────────"
+# echo "|Upgrading packages, this might take a while|"
+# echo "└─────────────────────────────────────────"
+# apt-get upgrade -yqq
 
 echo "┌─────────────────────────────────────────"
-echo "|Installing Nginx"
+echo "|Installing and configuring nginx"
 echo "└─────────────────────────────────────────"
-apt-get install nginx -yqq > /dev/null
+apt-get install nginx -yqq
 wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/default_nginx -O /etc/nginx/sites-enabled/default
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing dnsmasq"
+echo "└─────────────────────────────────────────"
+apt-get install dnsmasq -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring wlan0"
+echo "└─────────────────────────────────────────"
+wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dhcpcd.conf -O /etc/dhcpcd.conf
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring dnsmasq"
+echo "└─────────────────────────────────────────"
+wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/dnsmasq.conf -O /etc/dnsmasq.conf
+
+echo "┌─────────────────────────────────────────"
+echo "|configuring dnsmasq to start at boot"
+echo "└─────────────────────────────────────────"
+update-rc.d dnsmasq defaults
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing hostapd"
+echo "└─────────────────────────────────────────"
+apt-get install hostapd -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring hostapd"
+echo "└─────────────────────────────────────────"
+wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal/master/hostapd.conf -O /etc/hostapd/hostapd.conf
+sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring iptables"
+echo "└─────────────────────────────────────────"
+iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-destination 192.168.24.1:80
+iptables -t nat -A POSTROUTING -j MASQUERADE
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get -y install iptables-persistent
+
+echo "┌─────────────────────────────────────────"
+echo "|configuring hostapd to start at boot"
+echo "└─────────────────────────────────────────"
+systemctl unmask hostapd.service
+systemctl enable hostapd.service
 
 echo "┌─────────────────────────────────────────"
 echo "|Installing MySQL"
 echo "└─────────────────────────────────────────"
-apt-get install mysql-server -yqq > /dev/null
+apt-get install mariadb-server -yqq > /dev/null
 mysql_secure_installation <<EOF
 y
 Pa55w04d123
@@ -31,9 +94,10 @@ mysql -e "$dbsetup"
 echo "┌─────────────────────────────────────────"
 echo "|Installing PHP"
 echo "└─────────────────────────────────────────"
-apt-get install php-fpm php-mysql -yqq > /dev/null
-apt-get install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -yqq > /dev/null
-systemctl restart php7.0-fpm
+
+apt-get install php7.3-fpm php7.3-mbstring php7.3-mysql php7.3-curl php7.3-gd php7.3-curl php7.3-zip php7.3-xml -yqq > /dev/null
+
+systemctl restart php7.3-fpm
 
 echo "┌─────────────────────────────────────────"
 echo "|Installing Wordpress"
@@ -45,32 +109,6 @@ chown -R www-data:www-data /var/www/wordpress
 SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 STRING='put your unique phrase here'
 printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/wordpress/wp-config.php
-
-echo "┌─────────────────────────────────────────"
-echo "|Installing dnsmasq"
-echo "└─────────────────────────────────────────"
-apt-get install dnsmasq -yqq > /dev/null
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/dhcpcd.conf -O /etc/dhcpcd.conf
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/dnsmasq.conf -O /etc/dnsmasq.conf
-update-rc.d dnsmasq defaults
-
-echo "┌─────────────────────────────────────────"
-echo "|Installing hostapd"
-echo "└─────────────────────────────────────────"
-apt-get install hostapd -yqq > /dev/null
-wget -q https://raw.githubusercontent.com/tretos53/Captive-Portal-WordPress/master/hostapd.conf -O /etc/hostapd/hostapd.conf
-sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
-systemctl unmask hostapd.service
-systemctl enable hostapd.service
-
-echo "┌─────────────────────────────────────────"
-echo "|Configuring iptables"
-echo "└─────────────────────────────────────────"
-iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-destination 192.168.24.1:80
-iptables -t nat -A POSTROUTING -j MASQUERADE
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-apt-get -yqq install iptables-persistent > /dev/null
 
 echo "┌─────────────────────────────────────────"
 echo "|Reboot and test"
